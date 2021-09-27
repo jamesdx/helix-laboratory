f63215a06eb2b44bd0f8a6b898cf3207985cc0f8
zhe.d.wang <zhe.d.wang@accenture.com>
2021-09-27 10:49:51 +0800

    Build Spring Boot Application.

---
+++ src/main/java/org/helix/laboratory/HelixLaboratoryApplication.java
@@ -0,0 +1,11 @@
+package org.helix.laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixLaboratoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelixLaboratoryApplication.class,args);
+    }
+}
